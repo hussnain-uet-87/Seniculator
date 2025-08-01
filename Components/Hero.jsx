a5e8ebceb44ae72b1939d2394c12959ddf132800
hussnain-uet-87 <hussnainuet87@gmail.com>
2025-08-01 11:01:15 -0700

    Updated images import and usage

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import hero2 from "../src/assets/hero-img-2.png"
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
         <p className='text-xl mt-6'>Confused about your GPA? Stop guessing and start calculating! Seniculator makes it <span style={{fontFamily: "Schoolbell,cursive",color: "#bc4327"}} className='text-xl font-bold'>quick</span>, <span style={{fontFamily: "Schoolbell,cursive",color: "#5e9448"}} className='text-xl font-bold'>accurate</span>, and <span style={{fontFamily: "Schoolbell,cursive",color: "#84228a"}} className='text-xl font-bold '>stress-free </span>to find your GPA in just a few clicks. Whether you're prepping for scholarships, semester wrap-ups, or just keeping track, we’ve got your back. No sign-ups, no fuss.</p>
         <button style={{fontFamily: 'Poppins',color: "#4a3222"}} className='tracking-wide b-10 flex items-center gap-2 cursor-pointer font-bold px-8 py-3 bg-amber-200 border-amber-300 border-2 mt-5 rounded-full text-md mb-8'>Start Calculating <i class="ri-arrow-right-line"></i></button>
       </div>
-      <img src="../public/hero-img-2.png" className='w-xs md:w-md lg:w-1/2 float-right mr-6' alt="" />
+      <img src={hero2} className='w-xs md:w-md lg:w-1/2 float-right mr-6' alt="" />
       </div>
     </section>
   )
--- Components/HowToUse.jsx
+++ Components/HowToUse.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import num1 from "../src/assets/number-one.png"
+import num2 from "../src/assets/number-2.png"
+import num3 from "../src/assets/number-3.png"
+import handImage from "../src/assets/hand.png"
 
 const HowToUse = () => {
   return (
@@ -9,21 +13,21 @@ const HowToUse = () => {
                 <p className='mt-6 max-w-[400px]'>Getting your GPA has never been easier. Just follow these simple steps and you're good to go </p>
                 <div className='max-w-[500px] space-y-4 mt-6  flex flex-col'>
                     <div className='flex-1 flex items-center border-2 border-amber-400 bg-amber-100 p-3 md:p-5 rounded-3xl'>
-                        <img style={{boxShadow: "0px 0px 5px 3px #fbbf24" }} className='h-[30px] md:h-[50px]  rounded-3xl shadow-2xl shadow-amber-800' src="../public/number-one.png" alt="" />
+                        <img style={{boxShadow: "0px 0px 5px 3px #fbbf24" }} className='h-[30px] md:h-[50px]  rounded-3xl shadow-2xl shadow-amber-800' src={num1} alt="" />
                         <div className='ml-4  text-sm'>
                         <h3 className='font-bold'>Enter Your Grades</h3>
                         <p>Select the grades you've earned for each subject or course.</p>
                         </div>
                     </div>
                     <div className='flex-1 flex items-center bg-fuchsia-100 border-2 border-fuchsia-400 p-3 md:p-5 rounded-3xl'>
-                        <img style={{boxShadow: "0px 0px 5px 3px #e879f9" }} className='h-[30px] md:h-[50px]  rounded-3xl shadow-2xl shadow-amber-800' src="../public/number-2.png" alt="" />
+                        <img style={{boxShadow: "0px 0px 5px 3px #e879f9" }} className='h-[30px] md:h-[50px]  rounded-3xl shadow-2xl shadow-amber-800' src={num2} alt="" />
                         <div className='ml-4  text-sm'>
                         <h3 className='font-bold'>Add Credit Hours</h3>
                         <p>Enter the number of credit hours for each corresponding course.</p>
                         </div>
                     </div>
                     <div  className='flex-1 flex items-center border-2 border-sky-400 bg-sky-100 p-3 md:p-5 rounded-3xl'>
-                        <img style={{boxShadow: "0px 0px 5px 3px #7ed3f5" }} className='h-[30px] md:h-[50px] rounded-3xl shadow-2xl shadow-amber-800' src="../public/number-3.png" alt="" />
+                        <img style={{boxShadow: "0px 0px 5px 3px #7ed3f5" }} className='h-[30px] md:h-[50px] rounded-3xl shadow-2xl shadow-amber-800' src={num3} alt="" />
                         <div className='ml-4  text-sm'>
                         <h3 className='font-bold'>Click Calculate</h3>
                         <p>Instantly get your GPA — accurate, fast, and hassle-free.</p>
@@ -32,7 +36,7 @@ const HowToUse = () => {
                 </div>
             </div>
             <div className='flex-1 mt-[50px] md:mt-0 ml-6 flex items-center justify-center'>
-                <img src="../public/hand.png" className='lg:w-[400px] w-[250px]' alt="" />
+                <img src={handImage} className='lg:w-[400px] w-[250px]' alt="" />
             </div>
         </div>
     </section>
